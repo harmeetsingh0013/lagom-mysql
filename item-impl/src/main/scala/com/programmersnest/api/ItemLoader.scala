97b1de67cda6546a44384599cc007f4f70c832a8
harmeetsingh0013 <harmeetsingh.0013@gmail.com>
2017-05-18 22:42:35 +0530

    working on auto table generation in user modeul on mysql

---
+++ item-impl/src/main/scala/com/programmersnest/api/ItemLoader.scala
@@ -0,0 +1,19 @@
+package com.programmersnest.api
+
+/**
+  * Created by harmeet on 17/5/17.
+  */
+
+/*abstract class ItemComponents (context: LagomApplicationContext) extends LagomApplication(context)
+  with JdbcPersistenceComponents with HikariCPComponents with AhcWSComponents {
+
+  override lazy val jsonSerializerRegistry = UserJsonSerializerRegistry
+
+  override def lagomServer: LagomServer = serverFor[UserService](wire[UserServiceImpl])
+
+  persistentEntityRegistry.register(wire[I])
+}*/
+
+class ItemLoader {
+
+}
